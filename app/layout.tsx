2f2eb555a6f04e0f1d7e1597b1c8d143c1aab41d
Flopie966 <you@example.com>
2025-07-07 12:58:24 +0200

    webtab

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Moneybear',
+  description: 'Improved visual search of your second-hand items',
+  generator: 'Moneybear',
 }
 
 export default function RootLayout({
